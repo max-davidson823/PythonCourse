42aa6e75cd5a59164e221a06ce73ff6b494774cb
max-davidson823 <maxdavidson823@gmail.com>
2024-08-28 11:04:09 -0500

    chore: Refactor hot_cold.py game code for improved readability and error handling

--- hot_cold.py
+++ hot_cold.py
@@ -3,20 +3,24 @@ import os
 
 def rand_num(difficulty_choice):
     match difficulty_choice:
-        case '1':
+        case 1:
             rand_num = random.randint(1, 50)
+            print('Range: 1 - 50')
             print(rand_num)
             first_guess(rand_num)
-        case '2':
+        case 2:
             rand_num = random.randint(1, 100)
+            print('Range: 1 - 100')
             print(rand_num)
             first_guess(rand_num)
-        case '3':
+        case 3:
             rand_num = random.randint(1, 500)
+            print('Range: 1 - 500')
             print(rand_num)
             first_guess(rand_num)
-        case '4':
+        case 4:
             rand_num = random.randint(1, 1000)
+            print('Range: 1 - 1000')
             print(rand_num)
             first_guess(rand_num)
             
@@ -25,6 +29,10 @@ def first_guess(rand_num):
     if input_num.isdigit():
         input_num = int(input_num)
         difference = abs(rand_num - input_num)
+    else:
+        print('Invalid input')
+        first_guess(rand_num)
+        
         if difference == 0:
             print('You won!')
             exit()
@@ -42,7 +50,10 @@ def guess(rand_num, prev_difference):
     if guess_num.isdigit():
         guess_num  = int(guess_num)
         difference = abs(rand_num - guess_num)
-
+    else:
+        print('Invalid input')
+        guess(rand_num, prev_difference)
+        
         if difference == 0:
             print('You won!')
             exit()
@@ -62,24 +73,29 @@ def guess(rand_num, prev_difference):
     
         
 def main():
-    rand_num(difficulty_choice='2')
-    # print('Difficulty: 1')
-    # print('Score: 2')
-    # print('Exit: -1')
-    # menu_choice = input('Choice: ')
     
-    # if menu_choice == '1':
-    #     os.system('cls')
-    #     print('Easy: 1')
-    #     print('Medium: 2')
-    #     print('Hard: 3')
-    #     print('Extreme: 4')
-        
-    #     difficulty_choice = input('Choice: ')
-    #     rand_num(difficulty_choice)
-    # else:
-    #     print('Input not recognized')
-    #     exit()
-        
+    os.system('cls')
+    print('Easy: 1')
+    print('Medium: 2')
+    print('Hard: 3')
+    print('Extreme: 4')
+    print('Exit: 5')
+    
+    difficulty_choice = input('Choice: ')
+    if difficulty_choice .isdigit():
+        difficulty_choice = int(difficulty_choice)
+        if difficulty_choice not in range(1, 6):
+            print('Invalid choice')
+            main()
+            
+        if difficulty_choice == 5:
+            print('You have chosen to exit')
+            exit()
+        else:
+            rand_num(difficulty_choice)
+    else:
+        print('Invalid choice')
+        main()        
+
 if __name__ == "__main__":
     main()
